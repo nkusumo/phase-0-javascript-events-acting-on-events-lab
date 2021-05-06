d5fdeab22a43a2162b12c785392a143977e94a94
Nisa Kusumo <nis.kusumo@gmail.com>
2021-05-06 05:29:53 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const right = left + 40;
+
+    if (right < 400) {
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
